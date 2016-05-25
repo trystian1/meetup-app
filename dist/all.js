50f9b8b721af6533bc0428e9fb5d86cc4f906d22
trystian1 <trystian.offerman@gmail.com>
2016-05-25 09:25:02 +0200

    nitial commit

---
+++ dist/all.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('aaa', _react2.default);
+//# sourceMappingURL=all.js.map
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+// var gulp = require("gulp");
+// var sourcemaps = require("gulp-sourcemaps");
+// var babel = require("gulp-babel");
+// var concat = require("gulp-concat");
+// var browserify = require("gulp-browserify");
+//
+// gulp.task("default", function () {
+//   return gulp.src("app/**/*.js")
+//     .pipe(sourcemaps.init())
+//     .pipe(babel())
+//     .pipe(concat("all.js"))
+//     .pipe(sourcemaps.write("."))
+//     .pipe(gulp.dest("dist"));
+// });
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+
+function compile(watch) {
+  var bundler = watchify(browserify('./app/app.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
